5b434cf41191cba1630cbb84c63799052082d6b1
Orville Chomer <orville.chomer@gmail.com>
2019-08-12 12:03:59 -0500

    first fully working version of file

--- i-like-it.js
+++ i-like-it.js
@@ -1,5 +1,7 @@
 /*
    i-like-it.js
+   
+   expecting JQuery for some functionality...
 
 */
 
@@ -61,16 +63,162 @@ async function apiCall(cmd, dataPosted, fnSuccess,fnFailure) {
 // #####################################################################################################
 
 
+const LikeButtonInfoById = [];
+const LikeButtonIdsByIndex = [];
+
+
+
+
+
+/*************************************************************************
+  Call this once somewhere in the page setup
+  (buttons need to be part of markup before calling)
+ *************************************************************************/
+function setupPageLikeButtons() {
+  console.log("setupPageLikeButtons() called");
+  let sLikeButtonClassName = "likeBtn";
+  const ids = [];
+  
+  const buttons = $("."+sLikeButtonClassName);
+  const nMax = buttons.length;
+  
+  for (let n=0;n<nMax;n++) {
+    const button = buttons[n];
+    
+    button.style.width = "70px";
+    let sId = button.id;
+    
+    ids.push(sId);
+    
+    button.addEventListener('click', likeButtonClicked);
+  } // next n
+  
+  const iData = {};
+  
+  iData.buttonIds = ids;
+  
+  apiCall("likeButtonValues", iData, likeButtonValuesSuccess, likeButtonValuesFailure);
+  
+} // end of function setupPageLikeButtons() 
+
+
+
 
 
 /*************************************************************************
 
  *************************************************************************/
-function makeLikeButtonMarkup(sId) {
-  const s=[];
+function likeButtonValuesSuccess(returnedData) {
+  console.log("likeButtonValuesSuccess() called");
   
+  const likeButtonsFound = returnedData.likeButtonsFound;
+  const nMax = likeButtonsFound.length;
   
-} // end of function makeLikeButtonMarkup()
+  for (let n=0;n<nMax; n++) {
+    const buttonInfo = likeButtonsFound[n];
+    const button = $("#"+buttonInfo.buttonId)[0];
+    const sKey = buttonInfo.buttonKey;
+    let sCaption = "like";
+    let sCurrentValue;
+    
+    if (localStorage.getItem(sKey)) {
+      sCurrentValue = localStorage.getItem(sKey);
+      
+      if (sCurrentValue=== "liked") {
+        sCaption = "unlike";
+      } // end if
+    } // end if (localStorage.getItem(sKey)) 
+    
+    if (buttonInfo.likeCount > 0) {
+      sCaption = sCaption + " (" + buttonInfo.likeCount + ")";
+    } // end if
+    
+    button.innerHTML = sCaption;
+  } // next n
+  
+} // end of function likeButtonValuesSuccess()
+
+
+
+
+
+/*************************************************************************
+
+ *************************************************************************/
+function likeButtonValuesFailure(returnedData) {
+  console.log("likeButtonValuesFailure() called");
+} // end of function likeButtonValuesFailure()
+
+
+
+
+/*************************************************************************
+
+ *************************************************************************/
+function likeButtonClicked(evt) {
+  const button = evt.target;
+  const sKey = "likeBtn_"+button.id;
+  let sCurrentValue = "unliked"; // default
+  
+  if (localStorage.getItem(sKey)) {
+    sCurrentValue = localStorage.getItem(sKey);
+  } // end if
+  
+  if (sCurrentValue === "unliked") {
+    sCurrentValue = "liked";
+    button.innerHTML = "unlike";
+  } else {
+    sCurrentValue = "unliked"; 
+    button.innerHTML = "like";
+  } // end if/else
+  
+  const iData = {};
+  iData.buttonKey = sKey;
+  iData.buttonId = button.id;
+  iData.currentValue = sCurrentValue;
+  localStorage.setItem(sKey, sCurrentValue);
+  apiCall("likeButtonClicked", iData, likeButtonClickedSuccess, likeButtonClickedFailure);
+  
+} // end of function likeButtonClicked()
+
+
+
+/*************************************************************************
+
+ *************************************************************************/
+function likeButtonClickedSuccess(returnedData) {
+  console.log("likeButtonClickedSuccess() called");
+  console.log("returnedData.buttonId="+(returnedData.buttonId));
+  const button = $("#"+returnedData.buttonId)[0];
+  const sCurrentValue = localStorage.getItem(returnedData.buttonKey);
+  let sCaption = "??";
+  
+  if (sCurrentValue === "unliked") {
+    sCaption = "like";
+  } else {
+    sCaption = "unlike";
+  } // end if/else
+  
+  if (returnedData.likeCount > 0) {
+    sCaption = sCaption + " ("+(returnedData.likeCount)+")";
+  } // end if
+  
+  button.innerHTML = sCaption;
+  
+} // end of function likeButtonClickedSuccess()
+
+
+
+
+
+/*************************************************************************
+
+ *************************************************************************/
+function likeButtonClickedFailure(returnedData) {
+  console.log("likeButtonClickedFailure() called");
+} // end of function likeButtonClickedFailure()
+
+
 
 
 /*************************************************************************
